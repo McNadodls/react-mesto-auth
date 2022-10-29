5659bb42c62869a3d7052edb5485184b795a7734
McNadodls <103933912+McNadodls@users.noreply.github.com>
2022-10-29 14:08:22 +0300

    Delete src directory

--- src/contexts/CurrentUserContext.js
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-import {createContext} from 'react';
-
-=======
-import {createContext} from 'react';
-
->>>>>>> c7baf28ad85f6f5949ab4e0648b2a5ad4030a529
-export const CurrentUserContext = createContext();
